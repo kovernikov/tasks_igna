3644820ef46e4135162c1cd336b8a19b24bb91a2
Alexej <kovernikov@post.cz>
2021-06-07 01:36:04 +0200

    05_task_avatar

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Message from './Message';
 import AlternativeMessage from './AlternativeMessage';
+import src from './team_6.jpg';
 
 const messageData = {
-    avatar: 'https://scontent-prg1-1.xx.fbcdn.net/v/t1.18169-9/10415677_591647244282481_7781049486693802144_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=p-4coRc41Y8AX-5r0By&_nc_ht=scontent-prg1-1.xx&oh=135a412cff638dc43f97dc6a287f77ac&oe=60B5EA57',
+    avatar: src,
     name: 'Aleksey',
     message: 'npm start нажимал?',
     time: '22:00',
